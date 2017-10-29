e1833d145b690fefb06c9925d8bd7c0177361113
Jack-scl <scl_china@163.com>
2017-10-29 21:47:15 +0800

    将星期调整

--- resources/assets/js/components/common/Datepicker.vue
+++ resources/assets/js/components/common/Datepicker.vue
@@ -64,7 +64,7 @@
       }
 
       $(this.$el).find('.datepicker').datepicker({
-          dayNamesMin: ['一','二','三','四','五','六','日'],
+          dayNamesMin: ['日','一','二','三','四','五','六'],
           monthNames: ['1月','2月','3月','4月','5月','6月', '7月','8月','9月','10月','11月','12月'],
           dateFormat: 'yy-mm-dd',
           nextText: '>>',
